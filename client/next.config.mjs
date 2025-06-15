75c9a2fb88a82053c9aeeb5ac69caa01c8ce378c
Akashyadav1997 <akashyd11@gmail.com>
2025-06-16 02:09:12 +0530

    server url issue is now resolved

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	env:{
-		NEXT_PUBLIC_SERVER_URL: "http://localhost:7070"
+		// NEXT_PUBLIC_SERVER_URL: "http://localhost:7070",
+		NEXT_PUBLIC_SERVER_URL:"https://car-selling-platform.onrender.com/"
+
 	},
 	images: {
 		remotePatterns: [
